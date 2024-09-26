ea2c83e61bcd152f80928902f61e1beca9ac13cb
Nexabet1121 <nexabettoussaint@gmail.com>
2024-09-26 13:57:20 -0400

    Add files via upload

---
+++ P1HW1_ToussaintNexabet.py
@@ -0,0 +1,38 @@
+ # Nexabet Toussaint
+ # 9/24/2024
+ # P1HW1
+ # Make calculations
+print("-----Calculating Exponents----: ")
+print()
+print()
+
+# Get info from user
+base_value =  int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print()
+print()
+
+# Calculate the value of the exponent math
+result = base_value ** exponent
+# Display result of math
+print(base_value, "Raise to the power of", exponent, "is", result, "!!")
+print()
+print()
+print("-----Addition and Subtraction----: ")
+print()
+print()
+# Get info from user
+start_int =  int(input("Enter a starting integer: "))
+add_int = int(input("Enter an integer to add: "))
+sub_int = int(input("Enter an integer to substract: "))
+# Calculate the value of the addition and subtraction
+
+result2 = start_int + add_int - sub_int
+# Display result of math
+print()
+print()
+print(start_int, "+", add_int, "-", sub_int, "is equal to", result2)
+
+
+
+
